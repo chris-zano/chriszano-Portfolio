ec100d3b122c4df978a2803f2b59c8d80e8c7836
chris-zano <cncs.chris@gmail.com>
2022-02-28 15:35:06 +0000

    added mizta grills card and update to app.js

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ else {
 }
 
 function ready() {
-    const form = document.getElementById('contact-form');
-    form.addEventListener('submit',function(e){
-        
+    document.getElementById('le-cho').addEventListener('click',() => {
+        location.href = "https://chris-zano.github.io/Le-Cho/"
     })
+    document.getElementById('jsdocs').addEventListener('click',() => {
+       location.href = "https://chris-zano.github.io/documentation-page/"
+    })
+    document.getElementById('namegen').addEventListener('click',() => {
+       location.href = "https://chris-zano.github.io/Le-Cho/"
+    })
+    document.getElementById('carinsu').addEventListener('click',() => {
+       location.href = "https://chris-zano.github.io/Le-Cho/"
+    })
+    document.getElementById('survey').addEventListener('click',() => {
+       location.href = "https://chris-zano.github.io/survey_form/"
+    })
+    document.getElementById('miztaGrills').addEventListener('click',() => {
+        location.href = "https://chris-zano.github.io/miztaGrills/"
+     })
 }
